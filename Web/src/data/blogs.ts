32faef784d0f86e00876ac5cc21d75e95e49fff2
ruzzle <ruzzlebuhain.micronics@gmail.com>
2023-02-17 08:38:06 +0800

    resolve comments

--- Web/src/data/blogs.ts
+++ Web/src/data/blogs.ts
@@ -16,7 +16,7 @@ export const blogs = [
     thumbnail:
       "https://images.pexels.com/photos/936722/pexels-photo-936722.jpeg?auto=compress&cs=tinysrgb&w=600",
     author: "James",
-    numberOfVisits: "15",
+    numberOfVisits: 15,
   },
   {
     id: "9b5ceaba-a66f-43e7-b123-3e5eefd0b81f",
@@ -35,7 +35,7 @@ export const blogs = [
     thumbnail:
       "https://images.pexels.com/photos/164527/pexels-photo-164527.jpeg?auto=compress&cs=tinysrgb&w=600",
     author: "Ruzzle",
-    numberOfVisits: "10",
+    numberOfVisits: 10,
   },
   {
     id: "31802826-2248-4d44-ac87-d46766986bce",
@@ -54,6 +54,63 @@ export const blogs = [
     thumbnail:
       "https://images.pexels.com/photos/265087/pexels-photo-265087.jpeg?auto=compress&cs=tinysrgb&w=600",
     author: "Dave",
-    numberOfVisits: "31",
+    numberOfVisits: 31,
+  },
+  {
+    id: "1f5dbc34-8902-4d86-b60b-c2f695a05d9b",
+    title: "Title",
+    tags: [
+      {
+        description: "good",
+      },
+      {
+        description: "great",
+      },
+    ],
+    content:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+    dateCreated: new Date("2023-03-16"),
+    thumbnail:
+      "https://images.pexels.com/photos/265087/pexels-photo-265087.jpeg?auto=compress&cs=tinysrgb&w=600",
+    author: "Dave",
+    numberOfVisits: 31,
+  },
+  {
+    id: "5bfef272-0f99-4f28-bebf-dc5b78350bd5",
+    title: "Title",
+    tags: [
+      {
+        description: "good",
+      },
+      {
+        description: "great",
+      },
+    ],
+    content:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+    dateCreated: new Date("2023-03-16"),
+    thumbnail:
+      "https://images.pexels.com/photos/265087/pexels-photo-265087.jpeg?auto=compress&cs=tinysrgb&w=600",
+    author: "Dave",
+    numberOfVisits: 31,
+  },
+  {
+    id: "791be592-c9aa-4564-8808-a972cbf42b72",
+    title: "Title",
+    tags: [
+      {
+        description: "good",
+      },
+      {
+        description: "great",
+      },
+    ],
+    content:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+    dateCreated: new Date("2023-03-16"),
+    thumbnail:
+      "https://images.pexels.com/photos/265087/pexels-photo-265087.jpeg?auto=compress&cs=tinysrgb&w=600",
+    author: "Dave",
+    numberOfVisits: 31,
   },
 ];
--- Web/src/pages/Blogs/Blogs.tsx
+++ Web/src/pages/Blogs/Blogs.tsx
@@ -2,7 +2,9 @@ import React from "react";
 import "./Blogs.scss";
 import Banner from "library/Banner/Banner";
 import PageTitle from "library/PageTitle/PageTitle";
-import BlogsCard from "./components/BlogsCard";
+import BlogCard from "./components/BlogCard";
+import { Container, Grid } from "@mui/material";
+import { blogs } from "data/blogs";
 
 const Blogs: React.FC = () => {
   return (
@@ -13,7 +15,27 @@ const Blogs: React.FC = () => {
         title=""
         backgroundImage="https://images.pexels.com/photos/1105766/pexels-photo-1105766.jpeg?auto=compress&cs=tinysrgb&w=600"
       />
-      <BlogsCard />
+      <Container>
+        <Grid container spacing={2} marginBottom={3}>
+          {blogs.map((blog) => {
+            return (
+              <Grid
+                item
+                xs={6}
+                sm={6}
+                md={4}
+                lg={4}
+                margin={0}
+                padding={0}
+                width={2}
+                className="blog-grid"
+              >
+                <BlogCard {...blog} />
+              </Grid>
+            );
+          })}
+        </Grid>
+      </Container>
     </div>
   );
 };
