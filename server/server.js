69bfc5cad2583a518671b75255b18704b7aa8416
Dave Spencer Bacay <dave.bacay@gocfs.pro>
2023-06-30 07:36:34 +0800

    remove the root urls

--- server/server.js
+++ server/server.js
@@ -68,7 +68,7 @@ if (process.env.NODE_ENV === "production") {
     res.sendFile(path.resolve(__dirname, "client", "build", "index.html"))
   );
 } else {
-  app.get("/", (req, res) => {
+  app.get("/api", (req, res) => {
     res.send("API is running...");
   });
 }
