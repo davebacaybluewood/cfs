830c1b0d6d9cdb69d0853a234f305b795b24da9b
Darryle Bacay <darryle.bacay@gocfs.pro>
2023-07-04 07:27:32 +0800

    remove extra cards in carousel

--- client/src/pages/BusinessProtection/BusinessProtection.tsx
+++ client/src/pages/BusinessProtection/BusinessProtection.tsx
@@ -51,65 +51,114 @@ const BusinessProtection: React.FC = () => {
           {/* <Button variant="default">Learn More</Button> Please disregard, commented for future dev use */}
         </div>
       </Headline>
-      <Container className="feature-main-container">
-        <Grid container>
-          <Grid item xs={12} sm={12} md={12} lg={9}>
-            <ReactMultiCarousel
-              ref={carouselRef}
-              additionalTransfrom={0}
-              arrows={false}
-              autoPlaySpeed={3000}
-              centerMode={false}
-              className=""
-              containerClass="container-padding-bottom"
-              dotListClass=""
-              draggable
-              focusOnSelect={false}
-              infinite={true}
-              itemClass=""
-              keyBoardControl
-              minimumTouchDrag={80}
-              pauseOnHover
-              renderArrowsWhenDisabled={false}
-              renderButtonGroupOutside={false}
-              renderDotsOutside={false}
-              responsive={{
-                desktop: {
-                  breakpoint: {
-                    max: 3000,
-                    min: 1024,
-                  },
-                  items: 3,
-                  partialVisibilityGutter: 40,
-                },
-                mobile: {
-                  breakpoint: {
-                    max: 464,
-                    min: 0,
-                  },
-                  items: 1,
-                  partialVisibilityGutter: 30,
-                },
-                tablet: {
-                  breakpoint: {
-                    max: 1024,
-                    min: 464,
-                  },
-                  items: 2,
-                  partialVisibilityGutter: 30,
-                },
-              }}
-              rewind={false}
-              rewindWithAnimation={false}
-              rtl={false}
-              shouldResetAutoplay
-              showDots={false}
-              sliderClass=""
-              slidesToSlide={1}
-              swipeable
-            >
-              {featureDataBusiness?.map((data, index) => {
-                return (
+      <div className="plan">
+        <Container className="feature-main-container">
+          <Grid container>
+            {/* <Grid item xs={12} sm={12} md={12} lg={9}>
+                <ReactMultiCarousel
+                  ref={carouselRef}
+                  additionalTransfrom={0}
+                  arrows={false}
+                  autoPlaySpeed={3000}
+                  centerMode={false}
+                  className=""
+                  containerClass="container-padding-bottom"
+                  dotListClass=""
+                  draggable
+                  focusOnSelect={false}
+                  infinite={true}
+                  itemClass=""
+                  keyBoardControl
+                  minimumTouchDrag={80}
+                  pauseOnHover
+                  renderArrowsWhenDisabled={false}
+                  renderButtonGroupOutside={false}
+                  renderDotsOutside={false}
+                  responsive={{
+                    desktop: {
+                      breakpoint: {
+                        max: 3000,
+                        min: 1024,
+                      },
+                      items: 3,
+                      partialVisibilityGutter: 40,
+                    },
+                    mobile: {
+                      breakpoint: {
+                        max: 464,
+                        min: 0,
+                      },
+                      items: 1,
+                      partialVisibilityGutter: 30,
+                    },
+                    tablet: {
+                      breakpoint: {
+                        max: 1024,
+                        min: 464,
+                      },
+                      items: 2,
+                      partialVisibilityGutter: 30,
+                    },
+                  }}
+                  rewind={false}
+                  rewindWithAnimation={false}
+                  rtl={false}
+                  shouldResetAutoplay
+                  showDots={false}
+                  sliderClass=""
+                  slidesToSlide={1}
+                  swipeable
+                >
+                  {featureDataFamily?.map((data, index) => {
+                    return (
+                      <div
+                        style={{
+                          padding: "1rem",
+                        }}
+                      >
+                        <StandardCard
+                          key={index}
+                          icon={data.icon}
+                          title={data.title}
+                          description={data.description}
+                          button={{
+                            text: data.button.text,
+                          }}
+                        />
+                      </div>
+                    );
+                  })}
+                </ReactMultiCarousel>
+              </Grid> */}
+            {/* <Grid item xs={12} sm={12} md={12} lg={3}>
+                <div
+                  className="carousel-navigator-container"
+                  style={{
+                    padding: "1rem",
+                  }}
+                >
+                  <StandardCard>
+                    <div className="carousel-navigator">
+                      <h2>Other Features You May be Interested</h2>
+                      <div className="carousel-navigator-btn">
+                        <button
+                          onClick={(value) => carouselRef?.current?.previous()}
+                        >
+                          <BsChevronLeft />
+                        </button>
+                        <button
+                          onClick={(value) => carouselRef?.current?.next()}
+                        >
+                          <BsChevronRight />
+                        </button>
+                      </div>
+                    </div>
+                  </StandardCard>
+                </div>
+              </Grid> Future use*/}
+            {featureDataBusiness?.map((data, index) => {
+              return (
+                <Grid sm={12} md={4} lg={4}>
                   <div
                     style={{
                       padding: "1rem",
@@ -125,35 +174,12 @@ const BusinessProtection: React.FC = () => {
                       }}
                     />
                   </div>
-                );
-              })}
-            </ReactMultiCarousel>
+                </Grid>
+              );
+            })}
           </Grid>
-          <Grid item xs={12} sm={12} md={12} lg={3}>
-            <div
-              style={{
-                padding: "1rem",
-              }}
-            >
-              <div className="carousel-navigator-container">
-                <StandardCard>
-                  <div className="carousel-navigator">
-                    <h2>Other features you may be interested</h2>
-                    <button
-                      onClick={(value) => carouselRef?.current?.previous()}
-                    >
-                      <BsChevronLeft />
-                    </button>
-                    <button onClick={(value) => carouselRef?.current?.next()}>
-                      <BsChevronRight />
-                    </button>
-                  </div>
-                </StandardCard>
-              </div>
-            </div>
-          </Grid>
-        </Grid>
-      </Container>
+        </Container>
+      </div>
       <PlanBusiness />
       <SolutionBusiness />
       <ConsultationBusiness />
