d608e28e99e2686c81c89a423ca65b42dbfeae6b
Jerome Admana <jerome.admana@gocfs.pro>
2023-09-30 05:18:06 +0800

    api create delete and update

--- server/routes/merchandiseRoute.js
+++ server/routes/merchandiseRoute.js
@@ -1,12 +1,15 @@
 import express from "express";
 import {
-  getMerchandise, getMerchandiseById
+  createMerchandise, deleteMerchandise, getMerchandise, getMerchandiseById, updateMerchandiseDetails, updateMerchandiseStatus
 } from "../controllers/merchandiseController.js";
 import { protect } from "../middleware/authMiddleware.js";
 
 const router = express.Router();
 
+router.route("/").post(protect, createMerchandise);
+router.route("/:id").delete(protect, deleteMerchandise);
 router.route("/").get(protect, getMerchandise);
-router.route("/:id").get(protect, getMerchandiseById);
+router.route("/details/:id").put(protect, updateMerchandiseDetails);
+router.route("/status/:id").put(protect, updateMerchandiseStatus);
 
 export default router;
