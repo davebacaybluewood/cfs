18a29f0e6c667df0f185ea247d5cf89c7fe7bb3d
rei-gasis <rei.gasis@gmail.com>
2023-10-06 11:21:33 +0800

    API - Email Send Notification on Merchandise Transaction

--- server/routes/subscriptionRoutes.js
+++ server/routes/subscriptionRoutes.js
@@ -3,6 +3,7 @@ import {
   createSubscription,
   deleteSubscription,
   getSubscriptions,
+  emailRedeemMerchNotif,
 } from "../controllers/subscriptionControllers.js";
 import { adminAuth, agentAuth, protect } from "../middleware/authMiddleware.js";
 
@@ -12,5 +13,7 @@ router
   .post(createSubscription)
   .get(protect, adminAuth, getSubscriptions);
 router.route("/:_id").delete(protect, adminAuth, deleteSubscription);
+
+router.route("/redeem-merch").post(protect, emailRedeemMerchNotif);
 
 export default router;
