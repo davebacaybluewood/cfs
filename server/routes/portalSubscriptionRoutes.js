1e7c3f565381f39695160804dac592182684fbc8
Jerome Admana <jerome.admana@gocfs.pro>
2023-10-20 06:08:48 +0800

    add admin authentication

--- server/routes/portalSubscriptionRoutes.js
+++ server/routes/portalSubscriptionRoutes.js
@@ -1,12 +1,12 @@
 import express from "express";
 import { subscribeFreeTrial, getAllSubscribeFreeTrial } from "../controllers/portalSubscriptionController.js";
-import { protect, subscriberAuth } from "../middleware/authMiddleware.js";
+import { protect, subscriberAuth, adminAuth } from "../middleware/authMiddleware.js";
 
 const router = express.Router();
 
 router
     .route("/free-trial")
         .post(protect, subscriberAuth, subscribeFreeTrial)
-        .get(protect, getAllSubscribeFreeTrial);
+        .get(protect, adminAuth, getAllSubscribeFreeTrial);
 
 export default router;
