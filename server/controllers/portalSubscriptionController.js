20083018413977a51023d516e931eece5caf1cc5
Jerome Admana <jerome.admana@gocfs.pro>
2023-10-20 05:29:50 +0800

    GET API For Trial Subs

--- server/controllers/portalSubscriptionController.js
+++ server/controllers/portalSubscriptionController.js
@@ -1,6 +1,7 @@
 import expressAsync from "express-async-handler";
 import { API_RES_OK, API_RES_FAIL } from "../constants/constants.js";
 import PortalSubscription from "../models/portalSubscription.js";
+import Agent from "../models/agentModel.js";
 
 /* @desc:  Subscribe Free Trial
  * @route: POST /api/portal-subscription/free-trial
@@ -22,4 +23,64 @@ const subscribeFreeTrial = expressAsync(async (req, res) => {
   }
 });
 
-export { subscribeFreeTrial };
+/* @desc: Fetch Portal Subscription Details with Agent Information
+ * @route: GET /api/portal-subscription/free-trial
+ * @access: Private
+ */
+
+const getAllSubscribeFreeTrial = expressAsync(async (req, res) => {
+  try {
+    const portalSubscriptions = await PortalSubscription.find();
+
+    if (portalSubscriptions.length === 0) {
+      return res.status(404).json({ message: "Subscriber not found" });
+    }
+
+    const usersData = await Promise.all(
+      portalSubscriptions.map(async (portalSubscription) => {
+        const agent = await Agent.findOne({
+          userGuid: portalSubscription.userGuid,
+        });
+        if (agent) {
+          const dateCreated = portalSubscription.createdAt;
+          const expirationDate = new Date(dateCreated);
+          expirationDate.setDate(expirationDate.getDate() + 30);
+
+          const currentDate = new Date();
+          const daysRemaining = Math.max(
+            0,
+            Math.floor((expirationDate - currentDate) / (24 * 60 * 60 * 1000))
+          );
+
+          return {
+            _id: portalSubscription._id,
+            userGuid: portalSubscription.userGuid,
+            dateCreated: dateCreated,
+            expirationDate: expirationDate,
+            daysRemaining: daysRemaining,
+            firstName: agent.firstName,
+            lastName: agent.lastName,
+            emailAddress: agent.emailAddress,
+          };
+        }
+        return {
+          _id: portalSubscription._id,
+          userGuid: portalSubscription.userGuid,
+          dateCreated: portalSubscription.dateCreated,
+          expirationDate: portalSubscription.dateCreated,
+          daysRemaining: 0,
+          firstName: "Agent not found",
+          lastName: "Agent not found",
+          emailAddress: "Agent not found",
+        };
+      })
+    );
+
+    res.json(usersData);
+  } catch (err) {
+    console.error(err);
+    res.status(500).send(API_RES_FAIL("Server Error"));
+  }
+});
+
+export { subscribeFreeTrial, getAllSubscribeFreeTrial };
--- server/routes/portalSubscriptionRoutes.js
+++ server/routes/portalSubscriptionRoutes.js
@@ -1,9 +1,12 @@
 import express from "express";
-import { subscribeFreeTrial } from "../controllers/portalSubscriptionController.js";
+import { subscribeFreeTrial, getAllSubscribeFreeTrial } from "../controllers/portalSubscriptionController.js";
 import { protect, subscriberAuth } from "../middleware/authMiddleware.js";
 
 const router = express.Router();
 
-router.route("/free-trial").post(protect, subscriberAuth, subscribeFreeTrial);
+router
+    .route("/free-trial")
+        .post(protect, subscriberAuth, subscribeFreeTrial)
+        .get(protect, getAllSubscribeFreeTrial);
 
 export default router;
