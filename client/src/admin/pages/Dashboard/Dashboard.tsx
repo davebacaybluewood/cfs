6520154772b4a7a4fb3b9991470d1560a26f3b2b
Dave Spencer Bacay <dave.bacay@gocfs.pro>
2023-10-19 08:22:03 +0800

    add a calendly

--- client/src/admin/pages/Dashboard/Dashboard.tsx
+++ client/src/admin/pages/Dashboard/Dashboard.tsx
@@ -1,11 +1,12 @@
 import { paths } from "constants/routes";
 import Wrapper from "admin/components/Wrapper/Wrapper";
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 import { CrumbTypes } from "./types";
 import ConditionalBox from "./components/ConditionalBox/ConditionalBox";
 import { UserContext } from "admin/context/UserProvider";
 import useFetchUserProfile from "admin/hooks/useFetchProfile";
 import "./Dashboard.scss";
+import { PopupModal, PopupWidget } from "react-calendly";
 
 const crumbs: CrumbTypes[] = [
   {
@@ -26,9 +27,21 @@ const Dashboard: React.FC = () => {
   const USER_POSITION = profile?.position;
   const USER_ROLE = profile?.roles;
 
+  const [showCalendly, setShowCalendly] = useState(false);
+
   return (
     <Wrapper breadcrumb={crumbs} className="dashboard-content">
       <ConditionalBox position={USER_POSITION} roles={USER_ROLE} />
+      <PopupModal
+        url="https://calendly.com/gocfs/initial-bop-to-existing-agents-in-preparation-of-launching"
+        onModalClose={() => setShowCalendly((prevState) => !prevState)}
+        open={showCalendly}
+        /*
+         * react-calendly uses React's Portal feature (https://reactjs.org/docs/portals.html) to render the popup modal. As a result, you'll need to
+         * specify the rootElement property to ensure that the modal is inserted into the correct domNode.
+         */
+        rootElement={document.getElementById("root") as any}
+      />
     </Wrapper>
   );
 };
