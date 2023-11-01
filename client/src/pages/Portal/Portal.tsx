7e919cc90cf2e5ef67b60a008fc85110f932e960
Dave Spencer Bacay <dave.bacay@gocfs.pro>
2023-11-02 05:29:37 +0800

    remove unnecessary changes

--- client/src/pages/Portal/Portal.tsx
+++ client/src/pages/Portal/Portal.tsx
@@ -37,7 +37,7 @@ const Portal: React.FC = () => {
                 alignItems: "center",
               }}
             >
-              <h2 className="contentTitle">New Member</h2>
+              <h2 className="contentTitle">New Memeber</h2>
               <div style={{ display: "flex", gap: "10px" }}>
                 <a href="https://agent.comfortfinancialsolutions.com/signup">
                   Signup
