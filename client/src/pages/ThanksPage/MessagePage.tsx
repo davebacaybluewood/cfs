33f385e104f24ff7ae6db76b9608a505d67b11e3
darryle bacay <dmilesbacay@gmail.com>
2023-11-04 04:21:26 +0800

    replace content with actual one

--- client/src/pages/ThanksPage/MessagePage.tsx
+++ client/src/pages/ThanksPage/MessagePage.tsx
@@ -3,19 +3,25 @@ import "./MessagePage.scss";
 import { Container } from "@mui/material";
 
 const MessagePage: React.FC = () => {
-  return (
-    <Container>
-      <div className="message-page-container">
-        <h2>Unsubscribe</h2>
-        <p>Lorem ipsum dolor sit amet.</p>
-        <img src="/assets/images/templates/logout-img.png" alt="" />
-        <div className="message-captions">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit id odio
-          omnis ratione commodi cumque nisi amet maiores atque rem!
-        </div>
-      </div>
-    </Container>
-  );
+	return (
+		<Container>
+			<div className="message-page-container">
+				<div className="message-header">
+					<h2>Unsubscribe</h2>
+					<p>Lorem ipsum dolor sit amet.</p>
+				</div>
+				<div className="message-page-content">
+					<img src="/assets/images/templates/logout-img.png" alt="" />
+					<p>
+						You have been successfully unsubscribed from our email list. We're
+						sorry to see you go, but we respect your decision. If you ever
+						change your mind, you can resubscribe at any time. <br /> Thank you
+						for being a part of the CFS community
+					</p>
+				</div>
+			</div>
+		</Container>
+	);
 };
 
 export default MessagePage;
