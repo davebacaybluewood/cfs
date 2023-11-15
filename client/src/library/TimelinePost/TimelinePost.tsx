56c381058fafc276d46eef741e928e91d47de1f2
philcobsuzuki <philcobsuzuki@gmail.com>
2023-11-15 18:12:35 +0800

    added time ago library

--- client/src/library/TimelinePost/TimelinePost.tsx
+++ client/src/library/TimelinePost/TimelinePost.tsx
@@ -1,6 +1,6 @@
 import { Stack } from "@mui/material"
-import React from "react"
 import PostButtons from "./PostButtons"
+import TimeAgo from "react-timeago"
 
 interface TimelinePostProps {
   profileImg: string
@@ -19,23 +19,30 @@ const TimelinePost = ({
   content,
   imgContent,
 }: TimelinePostProps) => {
-  const testDate = new Date()
-  console.log(testDate)
   return (
     <div style={{ padding: "2rem", borderBottom: "2px solid #f2f2f2" }}>
       <Stack flexDirection={"row"} gap={2}>
-        <img
-          style={{ borderRadius: "50%", width: "5rem", height: "5rem" }}
-          src={profileImg}
-          alt=""
-        />
+        <div>
+          <img
+            style={{
+              borderRadius: "50%",
+              objectFit: "cover",
+              width: "6rem",
+              height: "6rem",
+            }}
+            src={profileImg}
+            alt=""
+          />
+        </div>
         <Stack flexDirection="column" gap={1}>
           <Stack flexDirection={"row"} gap={1} sx={{ textAlign: "left" }}>
             <h2>{fullName}</h2>
             <h2 style={{ color: "gray", fontWeight: 400 }}>@{userName}</h2>
             <h2 style={{ color: "gray" }}>·</h2>
-            {/* <ReactTimeAgo date={date} locale="en-US" timeStyle="twitter"/> */}
-            <h2 style={{ color: "gray", fontWeight: 400 }}>{datePosted}</h2>
+            <TimeAgo
+              date={datePosted}
+              style={{ color: "gray", fontWeight: 400, fontSize: "15px" }}
+            />
           </Stack>
           <p style={{ fontSize: "14px", fontWeight: 400, lineHeight: "20px" }}>
             {content}
--- client/src/pages/ProfilePage/fakePosts.tsx
+++ client/src/pages/ProfilePage/fakePosts.tsx
@@ -1,10 +1,10 @@
 export const fakePosts = [
   {
     profileImg:
-      "https://media.istockphoto.com/id/1470130937/photo/young-plants-growing-in-a-crack-on-a-concrete-footpath-conquering-adversity-concept.webp?b=1&s=170667a&w=0&k=20&c=IRaA17rmaWOJkmjU_KD29jZo4E6ZtG0niRpIXQN17fc=",
-    fullName: "Philcob Suzuki Josol",
+      "https://images.unsplash.com/photo-1506794778202-cad84cf45f1d?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    fullName: "Johnny Bravo",
     userName: "philcobsuzuki",
-    datePosted: "3m",
+    datePosted: "Aug 29, 2023",
     content: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo
             accusamus sapiente vero eaque? Placeat architecto cum optio.
             Delectus nobis, ut aut est temporibus possimus exercitationem dicta, 
@@ -15,7 +15,7 @@ export const fakePosts = [
       "https://media.istockphoto.com/id/1470130937/photo/young-plants-growing-in-a-crack-on-a-concrete-footpath-conquering-adversity-concept.webp?b=1&s=170667a&w=0&k=20&c=IRaA17rmaWOJkmjU_KD29jZo4E6ZtG0niRpIXQN17fc=",
     fullName: "Philcob Suzuki Josol",
     userName: "philcobsuzuki",
-    datePosted: "3m",
+    datePosted: "November 10, 2023",
     content: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo
             accusamus sapiente vero eaque? Placeat architecto cum optio.
             Delectus nobis, ut aut est temporibus possimus exercitationem dicta, 
@@ -26,7 +26,7 @@ export const fakePosts = [
       "https://media.istockphoto.com/id/1470130937/photo/young-plants-growing-in-a-crack-on-a-concrete-footpath-conquering-adversity-concept.webp?b=1&s=170667a&w=0&k=20&c=IRaA17rmaWOJkmjU_KD29jZo4E6ZtG0niRpIXQN17fc=",
     fullName: "Philcob Suzuki Josol",
     userName: "philcobsuzuki",
-    datePosted: "3m",
+    datePosted: "November 15, 2023",
     imgContent:
       "https://images.unsplash.com/photo-1600880292203-757bb62b4baf?q=80&w=1740&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
     content: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo
