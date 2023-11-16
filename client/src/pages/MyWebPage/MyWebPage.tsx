093d584ecbfbcbb5233697aa763feb8e31a876d7
darryle <dmilesbacay@gmail.com>
2023-11-17 03:51:45 +0800

    remove comment

--- client/src/pages/MyWebPage/MyWebPage.tsx
+++ client/src/pages/MyWebPage/MyWebPage.tsx
@@ -1,8 +1,8 @@
-import React, { useState } from "react"
-import MyWebPageWrapper from "./Layout/MyWebPageWrapper"
-import { Container, Grid } from "@mui/material"
-import useFetchUserProfile from "admin/hooks/useFetchProfile"
-import Spinner from "library/Spinner/Spinner"
+import React, { useState } from "react";
+import MyWebPageWrapper from "./Layout/MyWebPageWrapper";
+import { Container, Grid } from "@mui/material";
+import useFetchUserProfile from "admin/hooks/useFetchProfile";
+import Spinner from "library/Spinner/Spinner";
 import {
   FaFacebook,
   FaHome,
@@ -11,54 +11,54 @@ import {
   FaTwitter,
   FaCalendar,
   FaAddressCard,
-} from "react-icons/fa"
-import { HiLocationMarker } from "react-icons/hi"
-import { BsCalculator, BsChatRightTextFill } from "react-icons/bs"
-import { MdEmail, MdOutlineLibraryBooks } from "react-icons/md"
-import Button from "library/Button/Button"
-import { AiOutlineArrowRight } from "react-icons/ai"
-import { useNavigate, useParams } from "react-router-dom"
-import { Helmet } from "react-helmet"
-import { FiSend } from "react-icons/fi"
-import { GrSend } from "react-icons/gr"
-import { paths } from "constants/routes"
-import { CiGift, CiTrophy, CiVault } from "react-icons/ci"
-import "./MyWebPage.scss"
-import Timeline from "pages/MyWebPage/Timeline"
+} from "react-icons/fa";
+import { HiLocationMarker } from "react-icons/hi";
+import { BsCalculator, BsChatRightTextFill } from "react-icons/bs";
+import { MdEmail, MdOutlineLibraryBooks } from "react-icons/md";
+import Button from "library/Button/Button";
+import { AiOutlineArrowRight } from "react-icons/ai";
+import { useNavigate, useParams } from "react-router-dom";
+import { Helmet } from "react-helmet";
+import { FiSend } from "react-icons/fi";
+import { GrSend } from "react-icons/gr";
+import { paths } from "constants/routes";
+import { CiGift, CiTrophy, CiVault } from "react-icons/ci";
+import "./MyWebPage.scss";
+import Timeline from "pages/MyWebPage/Timeline";
 
 const MyWebPage: React.FC = () => {
-  const { user } = useParams()
-  const [content, setContent] = useState("home")
-  const [active, setActive] = useState(false)
+  const { user } = useParams();
+  const [content, setContent] = useState("home");
+  const [active, setActive] = useState(false);
 
-  const navigate = useNavigate()
+  const navigate = useNavigate();
 
   /* General Agent Information */
-  const userGuid = `${user}`
-  const { profile, loading } = useFetchUserProfile(userGuid)
+  const userGuid = `${user}`;
+  const { profile, loading } = useFetchUserProfile(userGuid);
 
   /*Personal information */
   const defaultAvatar =
-    "https://res.cloudinary.com/dfm2vczpy/image/upload/c_pad,b_auto:predominant,fl_preserve_transparency/v1688418199/cfs-image_rkkknx.jpg?_s=public-apps"
+    "https://res.cloudinary.com/dfm2vczpy/image/upload/c_pad,b_auto:predominant,fl_preserve_transparency/v1688418199/cfs-image_rkkknx.jpg?_s=public-apps";
   const avatar =
     profile?.avatar.toString() === "" || loading
       ? defaultAvatar
-      : profile?.avatar.toString()
+      : profile?.avatar.toString();
   const Agent = loading
     ? "CFS Agent"
-    : `${profile?.firstName} ${profile?.lastName}`
+    : `${profile?.firstName} ${profile?.lastName}`;
   const address =
-    profile?.state?.toString() === "" ? "-" : profile?.state?.toString()
-  const phoneNumber = profile?.phoneNumber.toString()
-  const email = profile?.emailAddress
+    profile?.state?.toString() === "" ? "-" : profile?.state?.toString();
+  const phoneNumber = profile?.phoneNumber.toString();
+  const email = profile?.emailAddress;
 
   /* Professional Information */
-  const licenseNumber = profile?.licenseNumber?.toString()
+  const licenseNumber = profile?.licenseNumber?.toString();
 
   /* Socials */
-  const facebook = profile?.facebook.toString()
-  const linkedIn = profile?.linkedIn.toString()
-  const twitter = profile?.twitter.toString()
+  const facebook = profile?.facebook.toString();
+  const linkedIn = profile?.linkedIn.toString();
+  const twitter = profile?.twitter.toString();
 
   const links = [
     {
@@ -81,14 +81,14 @@ const MyWebPage: React.FC = () => {
       title: "Twitter",
       link: twitter,
     },
-  ]
+  ];
 
   const navLinks = [
     {
       icon: <FaHome />,
       onClick: () => {
-        setContent("home")
-        setActive(true)
+        setContent("home");
+        setActive(true);
       },
       className: active === true && content === "home" ? "active-nav" : "",
       link: "Home",
@@ -96,8 +96,8 @@ const MyWebPage: React.FC = () => {
     {
       icon: <FaCalendar />,
       onClick: () => {
-        setContent("events")
-        setActive(true)
+        setContent("events");
+        setActive(true);
       },
       className: active === true && content === "events" ? "active-nav" : "",
       link: "Events",
@@ -105,8 +105,8 @@ const MyWebPage: React.FC = () => {
     {
       icon: <GrSend />,
       onClick: () => {
-        setContent("testimonial")
-        setActive(true)
+        setContent("testimonial");
+        setActive(true);
       },
       className:
         active === true && content === "testimonial" ? "active-nav" : "",
@@ -115,16 +115,16 @@ const MyWebPage: React.FC = () => {
     {
       icon: <MdOutlineLibraryBooks />,
       onClick: () => {
-        setContent("articles")
-        setActive(true)
+        setContent("articles");
+        setActive(true);
       },
       className: active === true && content === "articles" ? "active-nav" : "",
       link: "Articles",
     },
-  ]
+  ];
 
   if (loading) {
-    ;<Spinner variant="relative" />
+    <Spinner variant="relative" />;
   }
 
   return (
@@ -219,16 +219,6 @@ const MyWebPage: React.FC = () => {
                       </div>
                       <div className="tabs-content">
                         <Timeline content={content} />
-                        {/* /* These are all dummy, this must render the rightful component for each page. */}
-                        {/* {content === "home" ? (
-                          <Timeline content={content} />
-                        ) : content === "events" ? (
-                          <Timeline content={content} />
-                        ) : content === "articles" ? (
-                          <Timeline content={content} />
-                        ) : content === "testimonial" ? (
-                          <Timeline content={content} />
-                        ) : null} */}
                       </div>
                     </React.Fragment>
                     <div className="middle-col-content"></div>
@@ -289,7 +279,7 @@ const MyWebPage: React.FC = () => {
         </div>
       )}
     </MyWebPageWrapper>
-  )
-}
+  );
+};
 
-export default MyWebPage
+export default MyWebPage;
