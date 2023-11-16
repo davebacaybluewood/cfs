21b3377a1e59fbe6503c352166360f8589a555f7
philcobsuzuki <philcobsuzuki@gmail.com>
2023-11-17 03:15:07 +0800

    moved inline css to css file

--- client/src/library/TimelinePost/TimelinePost.tsx
+++ client/src/library/TimelinePost/TimelinePost.tsx
@@ -27,42 +27,17 @@ const TimelinePost = ({
   const [isContentFull, setIsContentFull] = useState(false)
 
   return (
-    <div
-      className={`timeline-post`}
-      style={{
-        padding: "2rem",
-        borderBottom: "2px solid #f2f2f2",
-        background: "white",
-      }}
-    >
+    <div className={`timeline-post`}>
       <Stack flexDirection={"row"} gap={2}>
         {profileImg && (
           <div>
-            <img
-              style={{
-                borderRadius: "50%",
-                objectFit: "cover",
-                width: "4.5rem",
-                height: "4.5rem",
-              }}
-              src={profileImg}
-              alt=""
-            />
+            <img className="profile-image" src={profileImg} alt="" />
           </div>
         )}
 
         <Stack flexDirection="column" gap={1} sx={{ position: "relative" }}>
           {tag && (
-            <div
-              className={`${tag === "article" && "position-top-left"}`}
-              style={{
-                background: "#ed3e4b",
-                marginRight: "auto",
-                padding: "5px 1rem",
-                color: "#fff",
-                fontWeight: 400,
-              }}
-            >
+            <div className={`${tag === "article" && "position-top-left"} tag`}>
               {tag.toUpperCase()}
             </div>
           )}
@@ -71,11 +46,7 @@ const TimelinePost = ({
           )}
           <Stack flexDirection={"row"} gap={1} sx={{ textAlign: "left" }}>
             <h2>{title}</h2>
-            {userName && (
-              <h2 style={{ color: "gray", fontWeight: 400, fontSize: "13px" }}>
-                @{userName}
-              </h2>
-            )}
+            {userName && <h2 className="username">@{userName}</h2>}
 
             <h2 style={{ color: "gray" }}>·</h2>
             <TimeAgo
@@ -84,28 +55,11 @@ const TimelinePost = ({
             />
           </Stack>
 
-          <p
-            style={{
-              fontSize: "13px",
-              fontWeight: 400,
-              lineHeight: "20px",
-              color: "#333",
-            }}
-          >
+          <p className="content">
             {isContentFull ? content : content.slice(0, 300)}
 
             {!isContentFull && content.length > 300 ? (
-              <span
-                onClick={() => setIsContentFull(true)}
-                style={{
-                  display: "inline-block",
-                  width: "auto",
-                  color: "blue",
-                  cursor: "pointer",
-                  marginLeft: "0.5rem",
-                  fontWeight: "300",
-                }}
-              >
+              <span className="see-more" onClick={() => setIsContentFull(true)}>
                 See more
               </span>
             ) : null}
