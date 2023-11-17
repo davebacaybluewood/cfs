b2cb7e443025774782cc4d8e24a8aee8bd09ae51
Mark Stephen <mark.zizon@gmail.com>
2023-11-17 16:13:19 +0800

    feat/api: achievements API endpoint

--- server/routes/achievementRoutes.js
+++ server/routes/achievementRoutes.js
@@ -1,9 +1,10 @@
 import express from "express";
-import { getAchievement } from "../controllers/achievementControllers.js";
+import { getUnitedNations, getOneYearTeam } from "../controllers/achievementControllers.js";
 import { protect } from "../middleware/authMiddleware.js";
 
 const router = express.Router();
 
-router.route("/united-nations").get(protect, getAchievement);
+router.route("/united-nations").get(protect, getUnitedNations);
+router.route("/one-year-team").get(protect, getOneYearTeam);
 
 export default router;
