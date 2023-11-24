181999cb5e2e9426a4745ecc1e2b3521d78470fd
philcobsuzuki <philcobsuzuki@gmail.com>
2023-11-24 10:29:18 +0800

    fixed merge conflicts

--- client/src/pages/MyWebPage/Timeline.tsx
+++ client/src/pages/MyWebPage/Timeline.tsx
@@ -1,37 +1,25 @@
-<<<<<<< HEAD
-import { useState, useEffect } from "react"
 import TimelinePost, {
   TimelinePostProps,
 } from "library/TimelinePost/TimelinePost"
-import { default as ApiAgent } from "api/agent"
-import { default as AdminAgent } from "admin/api/agent"
 import TimelineLoading from "./components/TimelineLoading"
-import { BlogData } from "pages/BlogPage/models"
-import Event from "admin/models/eventModel"
-=======
-import TimelinePost, { TimelinePostProps } from "library/TimelinePost/TimelinePost"
-import TimelineLoading from "./TimelineLoading"
->>>>>>> 1de7ab1eb01da6e01e23129e35d3b6fa933f9557
 
 interface TimelineProps {
-  userGuid?: string;
+  userGuid?: string
   data: TimelinePostProps[] | undefined
-  loading?: boolean;
+  loading?: boolean
 }
 const Timeline: React.FC<TimelineProps> = (props) => {
-  const { userGuid, data, loading } = props;
+  const { userGuid, data, loading } = props
 
   return (
     <div style={{ flexGrow: 1, width: "100%" }}>
       {loading ? (
         <TimelineLoading />
-      ) : data?.map((timeline) => {
-        return (
-          <TimelinePost
-            {...timeline}
-          />
-        )
-      })}
+      ) : (
+        data?.map((timeline) => {
+          return <TimelinePost {...timeline} />
+        })
+      )}
     </div>
   )
 }
