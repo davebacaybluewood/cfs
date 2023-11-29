56d284ceb0d78a3c2cb6ed2a4268bc34718191f5
darryle <dmilesbacay@gmail.com>
2023-11-30 03:36:36 +0800

    refactor

--- client/src/pages/MyWebPage/helpers/contactLinks.tsx
+++ client/src/pages/MyWebPage/helpers/contactLinks.tsx
@@ -1,3 +1,4 @@
+import React from 'react'
 import { FaPhone, FaAddressCard, FaLanguage } from 'react-icons/fa'
 import { HiLocationMarker } from 'react-icons/hi'
 import { MdEmail } from 'react-icons/md'
@@ -5,24 +6,24 @@ import { MdEmail } from 'react-icons/md'
 const contactLinks = (address: string, phoneNumber: string, email: string, licenseNumber: string, languages: string) => {
     return [
         {
-            icon: address === '' ? <></> : <HiLocationMarker />,
-            text: address === '' ? '' : address
+            icon: address ? <HiLocationMarker /> : '',
+            text: address ?? ''
         },
         {
             icon: phoneNumber === '' ? <></> : <FaPhone />,
-            text: phoneNumber === '' ? '' : phoneNumber
+            text: phoneNumber ?? ''
         },
         {
-            icon: email === '' ? <></> : <MdEmail />,
-            text: email === '' ? '' : email
+            icon: email ? <HiLocationMarker /> : '',
+            text: email ? email : ''
         },
         {
-            icon: licenseNumber === '' ? <></> : <FaAddressCard />,
-            text: licenseNumber === '' ? '' : licenseNumber
+            icon: licenseNumber ? <FaAddressCard /> : "",
+            text: licenseNumber ?? ''
         },
         {
-            icon: languages === '' ? <></> : <FaLanguage />,
-            text: languages === '' ? '' : languages
+            icon: languages ? <FaLanguage /> : '',
+            text: languages ?? ''
         },
     ]
 }
